d453ccf739610de82e69008ef40b324b2cc8acd8
aquamark-io <heyaquamark@gmail.com>
2025-05-08 15:47:27 -0700

    Update background.js

--- .github/workflows/background.js
+++ .github/workflows/background.js
@@ -1,32 +1,37 @@
 // background.js
 
-chrome.action.onClicked.addListener((tab) => {
-  // Get the user's email
-  chrome.identity.getProfileUserInfo((userInfo) => {
-    if (userInfo.email) {
-      console.log("User Email: ", userInfo.email);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'watermarkPDF') {
+    console.log(`Received request to watermark: ${request.filename}`);
 
-      // Send request to Lambda
-      fetch('https://jl47smynwk.execute-api.us-east-2.amazonaws.com/default/AquamarkHandler', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 
-          email: userInfo.email,
-          pdfData: "Base64EncodedPDFData" // We will update this next
+    // Get the Chrome user's email
+    chrome.identity.getProfileUserInfo((userInfo) => {
+      if (userInfo.email) {
+        console.log("User Email: ", userInfo.email);
+
+        // Send to Lambda
+        fetch('https://jl47smynwk.execute-api.us-east-2.amazonaws.com/default/AquamarkHandler', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: userInfo.email,
+            pdfData: request.data,
+            filename: request.filename
+          })
         })
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log("Lambda Response:", data);
-      })
-      .catch(error => {
-        console.error("Error calling Lambda:", error);
-      });
-
-    } else {
-      console.error("Failed to fetch user email.");
-    }
-  });
+          .then(response => response.json())
+          .then(data => {
+            console.log("Lambda Response:", data);
+            alert(`Watermarking Complete for ${request.filename}`);
+          })
+          .catch(error => {
+            console.error("Error calling Lambda:", error);
+          });
+      } else {
+        console.error("Failed to fetch user email.");
+      }
+    });
+  }
 });
